067461e71f28aa845032739aab912c614a79bad4
miguelAngeloSantana <65346317+miguelAngeloSantana@users.noreply.github.com>
2022-06-14 21:38:00 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const onmobile = window.document.getElementById("hamburguer");
-const clmobile = window.document.getElementById("mmain");
-
-onmobile.addEventListener('click', () => {
-    onmobile.classList.toggle('show');
-    clmobile.classList.toggle('show');  
-})
